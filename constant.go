445bb81bff8ea037e7e6894e646fb56b2d3bac6f
bhawanaSingh <bhawana.1441@gmail.com>
2018-04-09 18:09:25 +0530

    Create constant.go

---
+++ constant.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  
+const length int = 20
+const width int = 23
+const height int =10
+var volume int
+volume = length*width*height
+fmt.Println("Volume =", volume)   # Volume = 4600
+  
+}
