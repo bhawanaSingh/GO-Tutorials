6352ec03b6e1ddd8bb537e49b453dc988688693e
bhawanaSingh <bhawana.1441@gmail.com>
2018-04-09 17:27:15 +0530

    Update variable.go

--- variable.go
+++ variable.go
@@ -3,6 +3,6 @@
   var c float32 = 43.98
   var string1 string = " Thomos Robot"
   
-  fmt.Println("value of a =",a , "and b =",b,ch)  "# value of a = 10 and b = 20"
-  fmt.Println(c)                                  "# 43.98"
-  fmt.Println("My name is ",string1)              "# My name is Thomos Robot"
+  fmt.Println("value of a =",a , "and b =",b,ch)  # value of a = 10 and b = 20
+  fmt.Println(c)                                  # 43.98
+  fmt.Println("My name is ",string1)              # My name is Thomos Robot
