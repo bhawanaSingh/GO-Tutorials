3f84cf1b0aa6e65a604cb097a594ec56a9754db2
bhawanaSingh <bhawana.1441@gmail.com>
2018-04-09 22:55:09 +0530

    Create if-else.go

---
+++ if-else.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a,b int = 40, 50
+ if a> b && a!=b {
+ fmt.Println("a is greater than b")
+ } else if a<b && a!=b {
+ fmt.Println("a is less than b")
+ } else {
+ fmt.Println("a is equal to b")
+ }
+}
